d1a6ddc066e8e9f11037ab8f9696e5abde6b7efd
Valeriia Bugakova <93352917+bugashesh@users.noreply.github.com>
2021-10-29 02:39:01 +0300

    Add files via upload

---
+++ IAC.Bot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace IAC.Bot
+{
+    class Program
+    {
+        private static readonly string botToken = Environment.GetEnvironmentVariable("BOT_TOKEN");
+
+        private const string startCommand = "/start";
+        private const string randomCommand = "/random";
+
+        static void Main() => ConfigureBot().GetAwaiter().GetResult();
+
+        private static async Task ConfigureBot()
+        {
+            Console.WriteLine("Starting bot...");
+            BotClient.Instance.On(startCommand, HandleStartCommandAsync);
+            BotClient.Instance.On(randomCommand, HandleRandomAsync);
+            await BotClient.Instance.StartBotAsync(botToken);
+        }
+
+        private static async Task HandleStartCommandAsync(ITelegramBotClient client, Message message)
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "You used command: /start");
+        }
+
+        private static async Task HandleRandomAsync(ITelegramBotClient client, Message message)
+        {
+            var random = new Random();
+            await client.SendTextMessageAsync(message.Chat.Id, "Random number: " + random.Next(0, 101));
+        }
+    }
+}
